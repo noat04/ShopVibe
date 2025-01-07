88e914e8834376e8e79bfc03b1e24eb622060fe4
Mon0205 <nhuy02042003@gmail.com>
2025-01-07 12:47:20 +0700

    p

--- my-app/src/component/mainProduct/ProductList.js
+++ my-app/src/component/mainProduct/ProductList.js
@@ -5,16 +5,17 @@ import './productList.css';
 const ProductList = ({ data }) => {
     return (
         <div>
-            <div className='product-item'>
-                <div>
-                    <img src={data.image} alt={data.productName || 'Product'} />
+            <Link to={`/product-detail/${data.productId}`} className="product-link">
+                <div className='product-item'>
+                    <div>
+                        <img src={data.image} alt={data.productName || 'Product'} />
+                    </div>
+                    <div className="product-name">{data.productName}</div>
+                    <div className="product-price">{data.price}</div>
+
                 </div>
-                <div className="product-name">{data.productName}</div>
-                <div className="product-price">{data.price}</div>
-                <button className="btn-view-details">
-                    <Link to={`/product-detail/${data.productId}`} className="product-link">View</Link>
-                </button>
-            </div>
+            </Link>
+
 
         </div >
 
